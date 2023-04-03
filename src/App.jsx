379dc27ee561165da1f4f61261d286534545a5fd
Mariano <marinhoted@gmail.com>
2023-04-02 20:05:37 -0600

    Feat: Adding eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
-import './App.css';
+import './App.css'
+
+const newNotifications = [
+	{
+		id: '01',
+		user: 'Mark Webber',
+		type: 'REACTION_TO_POST',
+		postName: 'My first tournament today!',
+		date: '1m ago',
+	},
+	{
+		id: '02',
+		user: 'Angela Gray',
+		type: 'NEW_FOLLOWER',
+		date: '5m ago',
+	},
+	{
+		id: '03',
+		user: 'Jacob Thompson',
+		type: 'NEW_MEMBER_GROUP',
+		groupName: 'Chess Club',
+		date: '1 day ago',
+	},
+	{
+		id: '04',
+		user: 'Rizky Hasanuddin',
+		type: 'PRIVATE_MESSAGE',
+		message:
+			"Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+		date: '5 days ago',
+	},
+]
 
 function App() {
-  return <div className="App">Notification page</div>;
+	return (
+		<div className='App'>
+			<header>
+				<p>Notifications {newNotifications.length}</p>
+				<button>Mark all as read</button>
+			</header>
+			<main>
+				<ul>
+					<li>
+						<p>
+							Mark Webber reacted to your recent post My first tournament today!
+						</p>
+						<p>1m ago</p>
+					</li>
+				</ul>
+			</main>
+		</div>
+	)
 }
 
-export default App;
+export default App
